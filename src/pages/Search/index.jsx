d9990ec38fc43016c5597382a56903006355f7fc
ako2223 <akoval22@proton.me>
2024-06-05 20:20:35 +0600

    live search

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,83 +1,103 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Loader, Post} from "../../components";
-import {requests} from "../../api/requests.js";
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router';
-import SearchForm from '../../components/SearchForm/index.jsx';
-import {Link} from "react-router-dom";
-import ProfilePicture from '../../components/ProfilePicture/ProfilePicture.jsx';
-
+import React, { useEffect, useState } from "react";
+import { Button, Loader, Post } from "../../components";
+import { requests } from "../../api/requests.js";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import SearchForm from "../../components/SearchForm/index.jsx";
+import { Link } from "react-router-dom";
+import ProfilePicture from "../../components/ProfilePicture/ProfilePicture.jsx";
+import { useDebounce } from "@uidotdev/usehooks";
 
 const Search = () => {
-    const [searchResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState([]);
+  const [login2, setLogin2] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors },
+    reset,
+  } = useForm({ mode: "onChange" });
+  const [responseError, setResponseError] = useState("");
+  const navigate = useNavigate();
 
-    const {
-        register,
-        handleSubmit,
-        formState: {isValid, errors},
-        reset,
-    } = useForm({ mode: 'onChange' })
-    const [responseError, setResponseError] = useState("");
-    const navigate = useNavigate()
-
-    const submitRequestHandler = ({login}) => {
-        const data = {
-            login
+  const onInputChange = (event) => {
+    if (event) {
+      if (event.target.value && event.target.value) {
+        const value = event.target.value;
+        if (login2 != value) {
+          setLogin2(value);
+          //   debouncedonInputChange();
         }
+      }
+    } else null;
+  };
 
+  const searchUser2 = () => {
+    requests.getUserSearch(login2).then((res) => setSearchResults(res.data));
+  };
+  const debouncedonInputChange = useDebounce(login2, 500);
+  //   const debouncedonInputChange2 = useDebounce(onInputChange, 300);
+  const submitRequestHandler = ({ login }) => {
+    const data = {
+      login,
+    };
+  };
+
+  const searchUser = ({ login }) => {
+    requests.getUserSearch(login).then((res) => setSearchResults(res.data));
+  };
+
+  console.log(searchResults);
+
+  useEffect(() => {
+    if (login2) {
+      searchUser2();
     }
+  }, [debouncedonInputChange]);
 
-   const searchUser = ({login}) =>{
-    requests.getUserSearch(login).then(res => setSearchResults(res.data))
-    
-   }
-
-
-    console.log(searchResults)
-
-    // useEffect(() => {
-    //     requests.getUserSearch('test').then(res => setSearchResults(res.data))
-    // }, []);
-
-    // if(!searchResults) return <Loader/>
-    return (
-            <div className={"w-3/4 mx-auto border py-2 px-3 rounded bg-white/65"}>
-               
-               <SearchForm
-                formTitle={"Search users"}
-                handleSubmit={handleSubmit}
-                register={register}
-                errors={errors}
-                isValid={isValid}
-                reset={reset}
-                submitRequestHandler={searchUser}
-            />
-            <div className={"w-full flex justify-center mt-4"}>
-                <p className={"text-red-400 text-xl "}>{responseError}</p>
-            </div>
-                <div >
-                    
-                { 
-                    searchResults.map(i => (
-                      <div key={i.login} className={"mx-auto py-2 px-3 rounded bg-white/65 flex flex-row items-center"} key={i.login}>{i.login&&<>
-                      
-                      <ProfilePicture r={16}/>
-                        <Link
-                        className={"text-black-800 font-bold text-xl hover:text-blue-800 hover:underline text-center ml-5"}
-                        to={"/profile" }
-                    >{i.login}</Link></>
-                        
-                         
-                        }</div>
-                    ))
-                }
-
-</div>
-            </div>
-
-            
-      
-    );
+  // if(!searchResults) return <Loader/>
+  return (
+    <div className={"w-3/4 mx-auto border py-2 px-3 rounded bg-white/65"}>
+      <SearchForm
+        formTitle={"Search users"}
+        handleSubmit={handleSubmit}
+        register={register}
+        errors={errors}
+        isValid={isValid}
+        reset={reset}
+        submitRequestHandler={searchUser}
+        pre_function_input={onInputChange}
+      />
+      <div className={"w-full flex justify-center mt-4"}>
+        <p className={"text-red-400 text-xl "}>{responseError}</p>
+      </div>
+      <div>
+        {searchResults.map((i) => (
+          <div
+            key={i.login}
+            className={
+              "mx-auto py-2 px-3 rounded bg-white/65 flex flex-row items-center"
+            }
+            key={i.login}
+          >
+            {i.login && (
+              <>
+                <ProfilePicture />
+                <Link
+                  className={
+                    "text-black-800 font-bold text-xl hover:text-blue-800 hover:underline text-center ml-5"
+                  }
+                  to={"/profile"}
+                >
+                  {i.login}
+                </Link>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
