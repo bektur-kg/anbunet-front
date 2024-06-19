e0fa23d15af72bf6582e64486a0ba7865b790d64
ako2223 <akoval22@proton.me>
2024-06-19 23:17:15 +0600

    в Create теперь отлично работает preview, сделано красво относительно

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,9 +1,22 @@
 import React, {useState} from "react"
 import {CreatePostForm, CreateStoryForm} from "../../components"
-import {useForm} from "react-hook-form"
+import {set, useForm} from "react-hook-form"
 import {requests} from "../../api/requests.js"
 
 const Create = () => {
+
+    const [image, setImage] = useState('./128px-placeholder.png');  
+
+    const onImageChange = (event) => {
+        if (event) {
+          console.log(event);
+          if (event.target.files && event.target.files[0]) {
+            setImage(URL.createObjectURL(event.target.files[0]));
+            console.log('1')
+          } 
+         
+        } 
+      };
     const {
         register: postRegister,
         handleSubmit: postHandleSubmit,
@@ -24,6 +37,7 @@ const Create = () => {
         const formData = new FormData()
         formData.append("file", file[0])
         formData.append("description", description)
+        setImage('./128px-placeholder.png')
 
         requests
             .createPost(formData)
@@ -50,12 +64,18 @@ const Create = () => {
                 handleSubmit={postHandleSubmit}
                 isValid={isValidPostForm}
                 submitRequestHandler={createPostHandler}
+                pre_function_input={onImageChange}
             />
-            <div className={"text-red-400 mt-10 text-center"}>{responseError}</div>
+            <div className={"text-red-400 mt-2 text-center"}>{responseError}</div>
+           { image!=='./128px-placeholder.png'&&
+           
+            <div className={'flex justify-center'}> <img className={"max-w-sm    object-scale-down rounded-2xl"} alt="preview image" src={image} /></div>}
             {
                 isPostCreated &&
                 <div className={"text-emerald-400 text-xl mt-10 text-center"}>Post Created Successfully! </div>
+                
             }
+          
             <CreateStoryForm
                 register={storyRegister}
                 errors={storyFormErrors}
